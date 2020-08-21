d9f1b02104009c740d66a1ed0a41c4c61e946e44
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-08-21 17:25:09 -0500

    add business controller

---
+++ FrontEnd/Controllers/BusinessesController.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using FrontEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace FrontEnd.Controllers
+{
+  public class BusinessesController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allBusinesses = Business.GetBusinesses();
+      return View(allBusinesses);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Business business)
+    {
+      Business.Post(business);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var business = Business.GetDetails(id);
+      return View(business);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var business = Business.GetDetails(id);
+      return View(business);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Business business)
+    {
+      business.BusinessId = id;
+      Business.Put(business);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Business.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}
