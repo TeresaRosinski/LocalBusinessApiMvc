d0d48db08784810d9db98bfb05b38304a5d2ff6e
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-08-21 16:34:58 -0500

    add swagger

--- BusinessApi/Startup.cs
+++ BusinessApi/Startup.cs
@@ -11,7 +11,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
-using BusinessApi.Models; 
+using BusinessApi.Models;
+using Microsoft.AspNetCore.Mvc.ApiExplorer; //for swagger
+using Microsoft.AspNetCore.StaticFiles; // for swagger
+using System.Reflection; // for swagger
+using System.IO;// for swagger
+using Microsoft.OpenApi.Models; //for swagger
 
 namespace BusinessApi
 {
@@ -27,13 +32,33 @@ namespace BusinessApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BusinessApiContext>(opt => opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<BusinessApiContext>(opt=>opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            //Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Local Business API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
+            //Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            //Enable middleware to serve swagger-ui
+            //specify the Swagger JSON endpoint.
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty; // lets us go to local host and still view swagger page
+            });
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
