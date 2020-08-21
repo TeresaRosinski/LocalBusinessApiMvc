7990496def8435cdca26df380c77b131cd97274c
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-08-21 13:10:31 -0500

    add required parameters

--- BusinessApi/Models/Business.cs
+++ BusinessApi/Models/Business.cs
@@ -4,10 +4,14 @@ namespace BusinessApi.Models
   public class Business
   {
     public int BusinessId {get; set;}
+    [Required]
+    [StringLength(50)]
     public string Name {get; set;}
-
+    [Required]
     public string Type {get; set; }
+    [Required]
     public string Description {get; set;}
+    [Required]
     public string City {get; set;}
     
   }
